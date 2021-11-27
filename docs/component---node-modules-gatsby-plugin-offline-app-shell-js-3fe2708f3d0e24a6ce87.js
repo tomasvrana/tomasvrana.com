96163c6e63ee192b7f03888ba6740e290b400ff0
Tomas Vrana <tomas@vrana.org>
2021-11-27 01:14:59 +0100

    fix

---
+++ docs/component---node-modules-gatsby-plugin-offline-app-shell-js-3fe2708f3d0e24a6ce87.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{405:function(e,t,n){"use strict";var u=n(36);t.__esModule=!0,t.default=void 0;var o=u(n(29)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-3fe2708f3d0e24a6ce87.js.map
